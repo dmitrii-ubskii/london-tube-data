ff8892c13af46341b934e72b458138f6625b8839
Dmitrii Ubskii <ubskydm@gmail.com>
2022-03-30 13:41:08 +0300

    main.py: REPL interface

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+import readline
 
 import MySQLdb
 
@@ -65,7 +66,50 @@ if __name__ == '__main__':
         conn = MySQLdb.connect(user='dbuser', passwd='12345678')
         populate_db(conn)
 
-        print('> ')
+        cursor = conn.cursor()
+        while True:
+            command = input('> ')
+            if command == 'quit' or command == 'q':
+                break
+            # assume line
+            cursor.execute('SELECT id FROM TubeLines WHERE name = %s;', (command,))
+            result = cursor.fetchone()
+            if result is not None:  # it IS a line
+                line_id = result
+                cursor.execute('''
+                    SELECT name
+                    FROM TubeLineStations JOIN Stations
+                        ON (TubeLineStations.stationId = Stations.id)
+                    WHERE tubeLineId = %s;
+                ''', (line_id,))
+                print(f'Stations on the tube line "{command}":')
+                for station_name, in cursor.fetchall():
+                    print(f'    {station_name}')
+            else:  # not a line; a station?
+                cursor.execute('SELECT id FROM Stations WHERE name = %s;', (command,))
+                result = cursor.fetchone()
+                if result is not None:  # it IS a station
+                    station_id = result
+                    cursor.execute('''
+                        SELECT name
+                        FROM TubeLineStations JOIN TubeLines
+                            ON (TubeLineStations.tubeLineId = TubeLines.id)
+                        WHERE stationId = %s;
+                    ''', (station_id,))
+                    lines = cursor.fetchall()
+                    if len(lines) == 1:
+                        line_name = lines[0][0]
+                        print(f'The station {command} is located on the {line_name} line.')
+                    else:
+                        print(f'The station {command} is located on the following lines: ', end='')
+                        print(', '.join(map(lambda x: x[0], lines)))
+
+                else:  # not a station either
+                    print(f'ERROR: `{command}` is not a recognized line or station name.')
+
+
+    except EOFError:  # Ctrl+D to exit
+        pass
 
     finally:
         conn.query('DROP DATABASE London_Tube;')
